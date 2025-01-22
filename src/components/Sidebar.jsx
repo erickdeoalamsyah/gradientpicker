1245429f646b6ba3ac77088019177c7e67e1935b
Eric Deo Alamsyah <101803715+erickdeoalamsyah@users.noreply.github.com>
2025-01-22 19:25:59 +0700

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = ({ direction, setDirection, colors, setColors }) => {
   return (
     <div className=" rounded-lg shadow-lg border border-gray-500 py-4">
       <ColorPicker label="FIRST COLOR" color={colors[0]} setColor={(color) => setColors([color, colors[1]])} />
-      <ColorPicker label="SECOND" color={colors[1]} setColor={(color) => setColors([colors[0], color])} />
+      <ColorPicker label="SECOND COLOR" color={colors[1]} setColor={(color) => setColors([colors[0], color])} />
       <DirectionSelector direction={direction} setDirection={setDirection} />
       <RandomGradientButton setColors={setColors} />
       <ExportSection direction={direction} colors={colors} />
